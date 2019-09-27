d0c8ff4303d306d59a3f2e4e45f27ae0ae1a5a50
daos <daos@me.com>
2019-09-27 15:58:48 +0200

    Initial Commit

---
+++ DaoNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DaoNotes
+//
+//  Created by Denis Mikaya on 27.09.19.
+//  Copyright © 2019 Denis Mikaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
